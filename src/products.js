10568343311f8b2adc03e72a1aef1a055792fb0a
sharon <sharonarmin84@gmail.com>
2023-09-05 18:21:05 +0530

    start

---
+++ src/products.js
@@ -0,0 +1,45 @@
+import product1 from "./assets/product1.jpg"
+import product2 from "./assets/product2.jpg"
+import product3 from "./assets/product3.jpg"
+import product4 from "./assets/product4.jpg"
+import product5 from "./assets/product5.jpg"
+import product6 from "./assets/product6.jpg"
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Iphone",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id:2,
+        productName: "camera",
+        price: 2999.0,
+        productImage: product2,
+    },
+    {
+        id:3,
+        productName: "laptop",
+        price: 4999.0,
+        productImage: product3,
+    },
+    {
+        id:4,
+        productName: "ear buds",
+        price: 299.0,
+        productImage: product4,
+    },
+    {
+        id:5,
+        productName: "speaker",
+        price: 650.0,
+        productImage: product5,
+    },
+    {
+        id:6,
+        productName: "smart watch",
+        price: 750.0,
+        productImage: product6,
+    },
+]
